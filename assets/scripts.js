6bedee8120b91e9b6d5b63ee50d5631caec89c16
AkshayChn <chnakshay@gmail.com>
2020-05-02 01:56:31 +0530

    Added the page load overlay spinner

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,5 @@
 /* make a func to auto hide the sidebar on click on any element only for mobile */
+document.addEventListener("DOMContentLoaded", function() {
+  //console.log("Page Loaded Fully");
+  document.getElementById("overlay").style.visibility = "hidden";
+});
