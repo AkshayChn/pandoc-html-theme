bb75fb4425f65a2e41ab45b28196c36f2a491bd3
AkshayChn <chnakshay@gmail.com>
2020-05-02 23:52:13 +0530

    Changed the dark mode custom div colors

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,3 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
   //console.log("Page Loaded Fully");
   document.getElementById("overlay").style.visibility = "hidden";
 });
+
+//dark  mode checker
+var checkbox = document.querySelector("input[name=themeToggleCheckbox]");
+
+checkbox.addEventListener( 'change', function() {
+    if(this.checked) {
+        // Checkbox is checked..
+        console.log("Checked");
+        document.getElementById("bodyId").className = "theme-dark-1";
+    } else {
+        // Checkbox is not checked..
+        console.log("Unchecked");
+        document.getElementById("bodyId").className = "theme-light-1";
+        
+    }
+});
